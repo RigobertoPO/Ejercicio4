4ca5843321e190b57357975cc0d67194a4a05d3e
rigobertopo <rperez@cobach.edu.mx>
2024-10-04 18:46:40 -0600

    version

--- controlador.php
+++ controlador.php
@@ -4,22 +4,37 @@
     $operacion=$_POST['opcion'];
     include 'operaciones.php';
     $mate=new Matematicas($numero1,$numero2);
-    switch ($operacion) {
-        case '+':
-            echo 'La suma es ='.$mate->Suma();
-            break;
-        case '-':
-            echo 'La resta es ='.$mate->Resta();
-            break;
-        case '*':
-            echo 'La multiplicación es ='.$mate->Multiplica();
-            break;
-        case '/':
-            echo 'La división es ='.$mate->Division();
-            break;
-        default:
-            echo 'ocurrio un error';
-            break;
+    if($operacion=='+'){
+        echo 'La suma es ='.$mate->Suma();
     }
+    elseif($operacion=='-'){
+        echo 'La resta es ='.$mate->Resta();
+    }
+    elseif($operacion=='*'){
+        echo 'La multiplicación es ='.$mate->Multiplica();
+    }
+    elseif($operacion=='/'){
+        echo 'La división es ='.$mate->Division();
+    }
+    else{
+        echo 'ocurrio un error';
+    }
+    // switch ($operacion) {
+    //     case '+':
+    //         echo 'La suma es ='.$mate->Suma();
+    //         break;
+    //     case '-':
+    //         echo 'La resta es ='.$mate->Resta();
+    //         break;
+    //     case '*':
+    //         echo 'La multiplicación es ='.$mate->Multiplica();
+    //         break;
+    //     case '/':
+    //         echo 'La división es ='.$mate->Division();
+    //         break;
+    //     default:
+    //         echo 'ocurrio un error';
+    //         break;
+    // }
     
 ?>
